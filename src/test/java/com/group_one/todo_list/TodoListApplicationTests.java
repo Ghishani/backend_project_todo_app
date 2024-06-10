4e6e9c36666b15fb03baf83f2d2df53e5fda0cf7
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-10 14:11:32 +0100

    add initial working directory

---
+++ src/test/java/com/group_one/todo_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_one.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
