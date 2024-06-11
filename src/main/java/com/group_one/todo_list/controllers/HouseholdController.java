5789d4896918671012f8b86895ffcc89fed787aa
Gellila Zeru <gellilazeru1@gmail.com>
2024-06-11 12:32:25 +0100

    add post and patch mapping  housecontroller

--- src/main/java/com/group_one/todo_list/controllers/HouseholdController.java
+++ src/main/java/com/group_one/todo_list/controllers/HouseholdController.java
@@ -5,10 +5,7 @@ import com.group_one.todo_list.services.HouseholdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +33,22 @@ public class HouseholdController {
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
+    @PostMapping    //localhost:8080/households
+    public ResponseEntity<Household> addNewHousehold(@RequestBody Household household){
+        Household newHousehold = householdService.createHousehold(household);
+        return new ResponseEntity<>(newHousehold, HttpStatus.CREATED);
+    }
 
+    @PatchMapping(value = "/{id}") //localhost:8080/households/1
+    public ResponseEntity<Household> updateHousehold(@PathVariable Long id, @RequestBody Household household) {
+        Optional<Household> foundHousehold = householdService.getHouseholdById(id);
+        if (foundHousehold.isPresent()) {
+            Household updatedHousehold = householdService.updateHousehold(id, household);
+            return new ResponseEntity<>(updatedHousehold, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 
 
 }
